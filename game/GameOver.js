3153f7aaea05a8272c90dad8350488f5efbb48bb
skeleton100 <s1240046@u-aizu.ac.jp>
2018-08-28 05:22:32 +0900

    add README

--- game/GameOver.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", init);
-
-function init() {
-    GameOver = function() {
-        alert("ゲームオーバー");
-        createjs.Ticker.removeAllEventListeners();
-        stage.removeAllEventListeners();
-    }
-}
